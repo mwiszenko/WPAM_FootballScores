6a16ce99f35dffe0cfea9e9089650b9ab4e5c563
mwiszenko <wiszenkom@gmail.com>
2020-12-01 21:47:15 +0100

    Initial Commit

---
+++ FootballScores/FootballScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballScoresApp.swift
+//  FootballScores
+//
+//  Created by Michal on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FootballScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
